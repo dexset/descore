dfbb4a5929cfdacaa950a5234108f9a3e690e840
deviator <code.viator@gmail.com>
2014-06-27 21:00:21 +0400

    add algo unittest

--- import/desutil/algo.d
+++ import/desutil/algo.d
@@ -10,3 +10,11 @@ template amap(fun...) if ( fun.length >= 1 )
     auto amap(Range)(Range r) if (isInputRange!(Unqual!Range))
     { return array( map!fun(r) ); }
 }
+
+unittest
+{
+    int[] res = [ 1, 2, 3 ];
+    void func( int[] arr ) { res ~= arr; }
+    func( amap!(a=>a^^2)(res) );
+    assert( res == [ 1, 2, 3, 1, 4, 9 ] );
+}
