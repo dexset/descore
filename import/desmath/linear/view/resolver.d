165118f6490efff784ce24824b412be1f0e81032
deviator <code.viator@gmail.com>
2014-07-22 04:30:32 +0400

    rework camera

--- import/desmath/linear/view/resolver.d
+++ import/desmath/linear/view/resolver.d
@@ -22,55 +22,11 @@ The MIT License (MIT)
     THE SOFTWARE.
 +/
 
-module desmath.linear.node;
+module desmath.linear.view.resolver;
 
 public import desmath.linear.vector;
 public import desmath.linear.matrix;
-
-interface Node
-{
-    const @property
-    {
-        /+ local to parent transform +/
-        mat4 self();
-        const(Node) parent();
-
-        final
-        {
-            vec3 baseX() { return vec3( self.col!(0).data[0 .. 3] ); }
-            vec3 baseY() { return vec3( self.col!(1).data[0 .. 3] ); }
-            vec3 baseZ() { return vec3( self.col!(2).data[0 .. 3] ); }
-            /+ in parent system +/
-            vec3 basePoint() { return vec3( self.col!(3).data[0 .. 3] ); }
-        }
-    }
-}
-
-final class DimmyNode : Node
-{
-private:
-    Node owner;
-    mat4 mtr;
-
-public:
-    this( Node own = null ) { owner = own; }
-
-    @property
-    {
-        mat4 self() const { return mtr; }
-        mat4 self( in mat4 m ) { mtr = m; return mtr; }
-        const(Node) parent() const { return owner; }
-    }
-
-    void setParent( Node par ) { owner = par; }
-
-    void setPosition( in vec3 p )
-    {
-        mtr[0,3] = p.x;
-        mtr[1,3] = p.y;
-        mtr[2,3] = p.z;
-    }
-}
+public import desmath.linear.view.node;
 
 class Resolver
 {
@@ -85,10 +41,10 @@ class Resolver
         while( cam_branch[$-1] )
             cam_branch ~= cam_branch[$-1].parent;
 
-        top: 
-        foreach( cbi, camparents; cam_branch )
-            foreach( obi, objparents; obj_branch )
-                if( camparents == objparents )
+        top:
+        foreach( cbi, cam_parent; cam_branch )
+            foreach( obi, obj_parent; obj_branch )
+                if( cam_parent == obj_parent )
                 {
                     cam_branch = cam_branch[0 .. cbi];
                     obj_branch = obj_branch[0 .. obi];
@@ -98,10 +54,10 @@ class Resolver
         mat4 obj_mtr, cam_mtr;
 
         foreach( node; obj_branch )
-            obj_mtr = node.self * obj_mtr;
+            obj_mtr = node.matrix * obj_mtr;
 
         foreach( node; cam_branch )
-            cam_mtr = cam_mtr * node.self.speedTransformInv;
+            cam_mtr = cam_mtr * node.matrix.speedTransformInv;
 
         return cam_mtr * obj_mtr;
     }
