f52f07ca9e83bb5f92dedf86993fc502f7c0fb69
deviator <code.viator@gmail.com>
2015-02-18 22:22:30 +0300

    no changes, restruct code

--- import/des/util/arch/package.d
+++ import/des/util/arch/package.d
@@ -1,10 +1,9 @@
 module des.util.arch;
 
-public
-{
-    import des.util.arch.tree;
-    import des.util.arch.emm;
-    import des.util.arch.base;
-    import des.util.arch.sig;
-    import des.util.arch.slot;
-}
+public:
+
+import des.util.arch.tree;
+import des.util.arch.emm;
+import des.util.arch.base;
+import des.util.arch.sig;
+import des.util.arch.slot;
