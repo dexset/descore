3311da84bcceba794e6d41fa46fd63cd48924da8
deviator <code.viator@gmail.com>
2015-02-19 04:11:37 +0300

    add buildFlags function, fix #21

--- import/des/util/helpers.d
+++ import/des/util/helpers.d
@@ -1,6 +1,10 @@
 module des.util.helpers;
 
-import core.runtime, std.file, std.path;
+import core.runtime;
+import std.file;
+import std.path;
+import std.algorithm;
+
 
 /// `buildNormalizedPath`
 string bnPath( string[] path... ) 
@@ -19,3 +23,8 @@ string appPath( string[] path... )
 /// read text from app path file
 string readAPF( string[] path... )
 { return readText( appPath( path ) ); }
+
+/// convert array of values to bitmask
+auto buildFlags(T)( T[] list... )
+{ return reduce!((a,b)=>a|=b)(list); }
+
