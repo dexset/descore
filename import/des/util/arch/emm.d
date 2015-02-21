57847a3dbeef2303a85fc843950c802bce39a6bc
deviator <code.viator@gmail.com>
2015-02-21 03:40:50 +0300

    add if_orphan condition to registerChildEMM, fix #22

--- import/des/util/arch/emm.d
+++ import/des/util/arch/emm.d
@@ -2,9 +2,64 @@ module des.util.arch.emm;
 
 import des.util.arch.tree;
 
+import des.util.testsuite;
+
 ///
 interface ExternalMemoryManager : TNode!(ExternalMemoryManager,"","EMM")
 {
+    protected
+    {
+        ///
+        @property void isDestroyed( bool d );
+
+        ///
+        void selfConstruct();
+        ///
+        void selfDestroy();
+        ///
+        void preChildsDestroy();
+    }
+
+    ///
+    @property bool isDestroyed() const;
+
+    final
+    {
+        ///
+        T registerChildEMM(T)( T obj, bool if_orphan=false )
+            if( is( T == class ) || is( T == interface ) )
+        {
+            if( auto cemm = cast(ExternalMemoryManager)obj )
+                if( ( if_orphan && cemm.parentEMM is null ) || !if_orphan )
+                    attachChildsEMM( cemm );
+            return obj;
+        }
+
+        ///
+        T[] registerChildEMM(T)( T[] objs, bool if_orphan=false )
+            if( is( T == class ) || is( T == interface ) )
+        {
+            foreach( obj; objs )
+                registerChildEMM( obj, if_orphan );
+            return objs;
+        }
+
+        ///
+        T newEMM(T,Args...)( Args args )
+        { return registerChildEMM( new T(args) ); }
+
+        ///
+        void destroy()
+        {
+            if( isDestroyed ) return;
+            preChildsDestroy();
+            foreach( cemm; childsEMM )
+                cemm.destroy();
+            selfDestroy();
+            isDestroyed = true;
+        }
+    }
+
     ///
     mixin template EMM(string file=__FILE__,size_t line=__LINE__)
     {
@@ -12,7 +67,7 @@ interface ExternalMemoryManager : TNode!(ExternalMemoryManager,"","EMM")
         {
             protected enum __EMM_BASE_IMPLEMENT = true;
 
-            mixin TNodeHelperEMM!(true,true,true);
+            mixin TNodeHelperEMM!(true,true);
 
             private bool is_destroyed = false;
             public final bool isDestroyed() const { return is_destroyed; }
@@ -39,58 +94,81 @@ interface ExternalMemoryManager : TNode!(ExternalMemoryManager,"","EMM")
                 pragma(msg, format( "WARNING: duplicate mixin EMM at %s:%d", file, line ) );
         }
     }
-
-    protected
-    {
-        ///
-        @property void isDestroyed( bool d );
-
-        ///
-        void selfConstruct();
-        ///
-        void selfDestroy();
-        ///
-        void preChildsDestroy();
-    }
-
-    ///
-    @property bool isDestroyed() const;
-
-    final
-    {
-        ///
-        T registerChildEMM(T)( T obj )
-            if( is( T == class ) || is( T == interface ) )
-        {
-            auto cemm = cast(ExternalMemoryManager)obj;
-            if( cemm ) attachChildsEMM( cemm );
-            return obj;
-        }
-
-        ///
-        T[] registerChildEMM(T)( T[] objs )
-            if( is( T == class ) || is( T == interface ) )
-        {
-            foreach( obj; objs )
-                registerChildEMM( obj );
-            return objs;
-        }
-
-        ///
-        T newEMM(T,Args...)( Args args )
-        { return registerChildEMM( new T(args) ); }
-
-        ///
-        void destroy()
-        {
-            if( isDestroyed ) return;
-            preChildsDestroy();
-            foreach( cemm; childsEMM )
-                cemm.destroy();
-            selfDestroy();
-            isDestroyed = true;
-        }
-    }
 }
 
-version(todos) pragma(msg,__FILE__, "TODO: add unittests");
+unittest
+{
+    string[] log;
+
+    class Test : ExternalMemoryManager
+    {
+        mixin EMM;
+        string name;
+        this( string name ) { this.name = name; }
+    protected:
+        void preChildsDestroy()
+        { log ~= name ~ ".preChildsDestroy"; }
+        void selfDestroy()
+        { log ~= name ~ ".selfDestroy"; }
+    }
+
+    auto a = new Test( "a" );
+    auto b = new Test( "b" );
+    auto c = new Test( "c" );
+    auto d = new Test( "d" );
+
+    assertNull( a.parentEMM );
+    assertNull( b.parentEMM );
+    assertNull( c.parentEMM );
+    assertNull( d.parentEMM );
+
+    assertNull( a.childsEMM );
+    assertNull( b.childsEMM );
+    assertNull( c.childsEMM );
+    assertNull( d.childsEMM );
+
+    a.registerChildEMM( b );
+    assertEq( a.childsEMM.length, 1 );
+    assertEq( a.childsEMM[0], b );
+    assertEq( b.parentEMM, a );
+
+    c.registerChildEMM( b );
+    assertEq( a.childsEMM.length, 0 );
+    assertEq( c.childsEMM.length, 1 );
+    assertEq( c.childsEMM[0], b );
+    assertEq( b.parentEMM, c );
+
+    a.registerChildEMM( b, true );
+    assertEq( a.childsEMM.length, 0 );
+    assertEq( c.childsEMM.length, 1 );
+    assertEq( c.childsEMM[0], b );
+    assertEq( b.parentEMM, c );
+
+    a.registerChildEMM( b );
+    assertEq( a.childsEMM.length, 1 );
+    assertEq( a.childsEMM[0], b );
+    assertEq( b.parentEMM, a );
+    assertEq( c.childsEMM.length, 0 );
+
+    mustExcept({ b.registerChildEMM( a ); });
+
+    a.registerChildEMM( d );
+    assertEq( a.childsEMM.length, 2 );
+    assertEq( a.childsEMM[1], d );
+
+    d.destroy();
+    assertEq( a.childsEMM.length, 2 );
+    assertEq( log, [ "d.preChildsDestroy", "d.selfDestroy" ] );
+
+    assertNull( c.parentEMM );
+    assertNull( c.childsEMM );
+
+    a.destroy();
+    assertEq( log, [ "d.preChildsDestroy",
+                     "d.selfDestroy",
+                     "a.preChildsDestroy",
+                     "b.preChildsDestroy",
+                     "b.selfDestroy",
+                     "a.selfDestroy"
+                   ] );
+}
